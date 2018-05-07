61e04ca00e4a80c2e6ab54aa5f57c5a2fefbf51d
Mr Yang <logdns@gmail.com>
2018-05-07 18:05:35 +0800

    Update root-login-passwd.sh

--- bash/root-login-passwd.sh
+++ bash/root-login-passwd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo root:passwd |sudo chpasswd root
 sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^.*PubkeyAuthentication.*/PubkeyAuthentication no/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo reboot
