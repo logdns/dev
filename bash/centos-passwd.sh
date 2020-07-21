7732a6561fb145601aba0ea609f9c1912d80b902
Mr Yang <logdns@gmail.com>
2020-07-21 15:12:50 +0800

    Update centos-passwd.sh

--- bash/centos-passwd.sh
+++ bash/centos-passwd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "passwd" | passwd --stdin root > /dev/null;sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_c*;sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_c*;service sshd restart
+echo "passwd" | passwd --stdin root > /dev/null;sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config*;sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config*;service sshd restart
