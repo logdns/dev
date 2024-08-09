be3bbbc8f82530c0c28343bf0813a577600e0823
Walter <logdns@gmail.com>
2024-08-09 19:10:05 +0800

    Update disable_ssh_key_and_set_root_password.sh

--- bash/disable_ssh_key_and_set_root_password.sh
+++ bash/disable_ssh_key_and_set_root_password.sh
@@ -9,17 +9,19 @@ fi
 # 1. 备份当前的 SSH 配置文件
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
-# 2. 修改 sshd 配置文件，禁用PublicKeyAuthentication
+# 2. 修改 sshd 配置文件，禁用 PublicKeyAuthentication
 sed -i 's/^#*PubkeyAuthentication.*/PubkeyAuthentication no/' /etc/ssh/sshd_config
 
 # 3. 重新启动 SSH 服务以应用更改
 systemctl restart sshd
 
-# 4. 删除已配置的所有SSH keys (可选步骤)
-# 请注意，这会删除当前用户的所有已配置的SSH keys，如果你想保留某些用户的 key，可以调整以下路径或用for循环处理用户列表
+# 4. 删除 root 账户的已配置的所有 SSH keys
 rm -rf /root/.ssh/authorized_keys
-# 如果需要，逐个用户删除 .ssh 目录内的 authorized_keys 文件，以下为示例：
-# for user in $(ls /home); do rm -rf /home/$user/.ssh/authorized_keys; done
+
+# 逐个用户删除 .ssh 目录内的 authorized_keys 文件，这里遍历所有用户目录
+for user in $(ls /home); do 
+    rm -rf /home/$user/.ssh/authorized_keys; 
+done
 
 # 5. 设置 root 密码
 echo 'root:qq123456789' | chpasswd
