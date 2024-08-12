2bf45f2d702e83c20aebf0a977a18da3dc0bcc33
Walter <logdns@gmail.com>
2024-08-12 13:55:19 +0800

    Update disable_ssh_key_and_set_root_password.sh

--- bash/disable_ssh_key_and_set_root_password.sh
+++ bash/disable_ssh_key_and_set_root_password.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo root:Asdf123456789 |sudo chpasswd root
+echo root:abcxyz123456789 |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo reboot
