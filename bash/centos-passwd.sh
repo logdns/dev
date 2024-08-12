6a63a04224e8455706da1d6c818b5ddcf8649d14
Walter <logdns@gmail.com>
2024-08-12 13:56:16 +0800

    Delete bash/centos-passwd.sh

--- bash/centos-passwd.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "passwd" | passwd --stdin root > /dev/null;sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config*;sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config*;service sshd restart
