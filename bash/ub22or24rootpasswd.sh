c204f34862b025013b102574d53a86518871bb6d
Walter <logdns@gmail.com>
2024-10-10 14:23:15 +0800

    Update ub22or24rootpasswd.sh

--- bash/ub22or24rootpasswd.sh
+++ bash/ub22or24rootpasswd.sh
@@ -4,17 +4,17 @@
 echo "root:123123" | sudo chpasswd
 
 # 屏蔽 Include 行
-sudo sed -i 's/^Include \/etc\/ssh\/ssh_config.d\/\*.conf/#Include \/etc\/ssh\/ssh_config.d\/\*.conf/g' /etc/ssh/sshd_config
+sed -i 's/^Include \/etc\/ssh\/ssh_config.d\/\*.conf/#Include \/etc\/ssh\/ssh_config.d\/\*.conf/g' /etc/ssh/sshd_config
 
 # 设置 PermitRootLogin 和 PasswordAuthentication
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 # 设置 iptables 规则
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -F
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
 
 # 重启 SSH 服务
-sudo systemctl restart sshd
+systemctl restart sshd
