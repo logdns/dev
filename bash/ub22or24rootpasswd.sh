bd4ff36aa14c79b4b4860ce32ca89efea8dafc99
Walter <logdns@gmail.com>
2024-10-10 14:34:55 +0800

    Update ub22or24rootpasswd.sh

--- bash/ub22or24rootpasswd.sh
+++ bash/ub22or24rootpasswd.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-
 # 修改 root 密码
 echo "root:123123" | chpasswd
 
-# 屏蔽 Include 行
-sed -i 's/^Include \/etc\/ssh\/ssh_config.d\/\*.conf/#Include \/etc\/ssh\/ssh_config.d\/\*.conf/g' /etc/ssh/sshd_config
-
 # 设置 PermitRootLogin 和 PasswordAuthentication
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-
-# 设置 iptables 规则
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -F
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config.d/*; sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config.d/*
 
 # 重启 SSH 服务
-systemctl restart sshd
+systemctl restart ssh sshd
